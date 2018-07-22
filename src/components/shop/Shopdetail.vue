90855f6ddb95ca2d028375507987b81737c33248
greenlilys <wdwhqs163@qq.com>
2018-07-22 23:55:24 +0800

    页面字体优化

--- src/components/shop/Shopdetail.vue
+++ src/components/shop/Shopdetail.vue
@@ -9,39 +9,39 @@
 					</div>
 				</div>
 				<div class="baseCenter">
-					<div class="font-16">所属运营商：郑州第一运营中心（TNC0001）</div>
+					<div class="font-14">所属运营商：郑州第一运营中心（TNC0001）</div>
 				</div>
-				<div class=" flex-wrap flex-horizontal flex-align-center flex-justify-end baseRight">
+				<div class=" flex-wrap flex-horizontal flex-align-center flex-justify-end baseRight font-14">
 					<div class="stage"><span>网点状态：</span><span>上线中</span></div>
-					<el-button type="primary" size="small" class="btnStyle font-16">下&nbsp;&nbsp;&nbsp;线</el-button>	
-					<el-button type="primary" size="small" class="btnStyle font-16" @click="navChangeShopInfo">查看/修改网点信息</el-button>	
+					<el-button type="primary" size="small" class="btnStyle">下&nbsp;&nbsp;&nbsp;线</el-button>	
+					<el-button type="primary" size="small" class="btnStyle" @click="navChangeShopInfo">查看/修改网点信息</el-button>	
 				</div>
 			</div>	
 			
 			<div class="numInfo flex-wrap flex-horizontal flex-justify-between">
 				<div class="account tc">
 					<h1 class="font-16 colorYellow">账号管理</h1>
-					<div class="font-16 mt-10">账号：18843411171&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;启用中</div>
+					<div class="font-14 mt-10">账号：18843411171&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;启用中</div>
 					<el-button type="primary" size="small" class="btnStyle mt-10">确&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;定</el-button>	
 				</div>
 				<div class="tc ">
 					<h1 class="font-16 colorYellow">电池库存</h1>
-					<div class="font-16 mt-10">60&20&nbsp;&nbsp;15/15组&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;72&20&nbsp;&nbsp;10/12组</div>
+					<div class="font-14 mt-10">60&20&nbsp;&nbsp;15/15组&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;72&20&nbsp;&nbsp;10/12组</div>
 					<el-button type="primary" size="small" class="btnStyle mt-10">+&nbsp;补货记录</el-button>	
 				</div>
 				<div class="tc">
 					<h1 class="font-16 colorYellow">配货成本</h1>
-					<h2 class="font-16 mt-20 ">￥15200.00</h2>
+					<h2 class="font-14 mt-20 ">￥15200.00</h2>
 				</div>
 				<div class="tc">
 					<h1 class="font-16 colorYellow">钱包余额</h1>
-					<div class="font-16 mt-10">￥3564.00</div>
+					<div class="font-14 mt-10">￥3564.00</div>
 					<el-button type="primary" size="small" class="btnStyle mt-10">充&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;值</el-button>
 					<el-button type="primary" size="small" class="btnStyle mt-10">扣&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;款</el-button>
 				</div>
 				<div class="tc">
 					<h1 class="font-16 colorYellow">退换回收电池</h1>
-					<div class="font-16 mt-10">60&20&nbsp;&nbsp;5组&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;72&20&nbsp;&nbsp;10组</div>
+					<div class="font-14 mt-10">60&20&nbsp;&nbsp;5组&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;72&20&nbsp;&nbsp;10组</div>
 					<el-button type="primary" size="small" class="btnStyle mt-10">+&nbsp;回收记录</el-button>	
 				</div>
 			</div>
@@ -111,12 +111,12 @@
 	.leftName span:nth-child(1){margin-right:10px;}
 	.star img{display:inline-block;height:30px;width:30px;margin-right:4px;}
 	.baseCenter div{text-align:center;height:80px;line-height:80px;}
-	.baseRight{width:40%;font-size:16px;}
+	.baseRight{width:40%;}
 	.baseRight div:nth-child(1),.baseRight div:nth-child(2){margin-right:10px;}
 	.baseRight div:nth-child(2),.baseRight div:nth-child(3){cursor: pointer;}
 	.stage span:nth-child(2){color:#FF9900;margin-right:20px;}
 	
-	.numInfo>div{background:#fff;margin-right:10px;flex:1;margin-top:10px;padding:10px 20px;}
+	.numInfo>div{background:#fff;margin-right:10px;flex:1;margin-top:10px;padding:10px 0px;}
 	.numInfo div:nth-child(5){margin-right:0;}
 	
 	.conBottom{width:100%;padding:10px;background:#fff;position:relative;}
